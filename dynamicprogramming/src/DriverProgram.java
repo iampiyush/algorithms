f613346dc81bbd3099c633a988c74c0012802b1c
Ryan Reeves <rreevessde@gmail.com>
2011-05-15 14:20:15 -0700

    Updated driver to use Subset class

--- dynamicprogramming/src/DriverProgram.java
+++ dynamicprogramming/src/DriverProgram.java
@@ -1,60 +1,5 @@
-import java.lang.IllegalArgumentException;
-
-class Knapsack {
-    private int []mWeights;
-    private int []mValues;
-    private int mLength;
-    private int mAllowedWeight;
-
-    public Knapsack(int[] weights, int []values, int allowedWeight) {
-	if (weights.length != values.length)
-	    throw new IllegalArgumentException();
-	
-	mAllowedWeight = allowedWeight;
-	mWeights = weights;
-	mValues = values;
-	mLength = mValues.length;
-    }
-    
-    /*
-      Recsurive version of the knapsack algorithm with
-      memoization.
-      
-      Results from recusrive calls are saved in a table. Subsequent 
-      calls to the same subproblem are retrieved from the table instead
-      of recomputing them.
-     */
-    public int fillSackFaster() {
-	return 0;
-    }
-      /*
-	Recursive version of the knapsack algorithm with no 
-	memoization.
-
-	Pick items from the mValues array that are at most
-	mAllowedWeight. Values put in the sack should maximize 
-	value.
-     */
-    public int fillSackSlow() {
-	return fillSackRecursive(mAllowedWeight,0);
-    }
-
-    private int fillSackRecursive(int allowedWeight, int i) {
-	if (i == mLength) 
-	    return 0;
-
-	if (mWeights[i] > allowedWeight) {
-	    return fillSackRecursive(allowedWeight, i+1);
-	} 
-	
-	int next = fillSackRecursive(allowedWeight, i+1);
-
-	int newWeight = allowedWeight - mWeights[i];
-	int current = mValues[i] + fillSackRecursive(newWeight, i+1);
-
-	return Math.max(next, current);
-    }
-}
+import com.rreeves.dp.Subsets;
+import com.rreeves.dp.Knapsack;
 
 public class DriverProgram {
     public static void main(String []args) {
@@ -66,5 +11,11 @@ public class DriverProgram {
 	int max = ks.fillSackSlow();
 	
 	System.out.println("Maximum combination of values in sack is " + max);
+
+	String str = "abcd";
+	Subsets c = new Subsets();
+	c.printTopdown(str);
+
+	c.printBottomup(str);
     }
 }
