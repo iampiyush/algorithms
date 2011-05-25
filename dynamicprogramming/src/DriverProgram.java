f2dfa4a3f62258d25b926862f7b511e555a59d24
Ryan Reeves <rreevessde@gmail.com>
2011-05-24 21:29:55 -0700

    Adding version of knapsack that evaluates string starting at the end

--- dynamicprogramming/src/DriverProgram.java
+++ dynamicprogramming/src/DriverProgram.java
@@ -2,26 +2,37 @@ import com.rreeves.dp.Subsets;
 import com.rreeves.dp.Knapsack;
 
 public class DriverProgram {
-    public static void main(String []args) {
-	int []values = {19, 2, 9, 8, 99};
-	int []weight = {4, 2, 1, 1 ,5};
-	int capacity = 6;
 
-	Knapsack ks = new Knapsack(weight, values, capacity);
-	int max = ks.fillSackSlow();
-	
-	System.out.println("Maximum combination of values in sack is " + max);
-
+    private static void printSubsets() {
 	String str = "abcde";
 	Subsets c = new Subsets();
 	
 	System.out.println("Print top down");
-	//c.printTopdown(str);
+	c.printTopdown(str);
 	
 	System.out.println("\n\nPrint bottom up\n\n");
-	//c.printBottomup(str);
+	c.printBottomup(str);
+
+    }
+    
+    private static void knapsackTest() {
+	int []values = {19, 2, 9, 8, 9};
+	int []weight = {4, 2, 1, 1 ,5};
+	int capacity = 5;
+
+	Knapsack ks = new Knapsack(weight, values, capacity);
+	int max = ks.fillSackSlow();
+	System.out.println("KS 1 - Maximum combination of values in sack is " + max);
+
+	Knapsack2 ks2 = new Knapsack2(weight, values, capacity);
+	max = ks2.fillSack();
+	System.out.println("KS 2 - Maximum combination of values in sack is " + max);
 
 	max = ks.fillSackFaster();
 	System.out.println("Max = " + max);
+    }
+
+    public static void main(String []args) {
+	knapsackTest();
     }
 }
---
+++ dynamicprogramming/src/com/rreeves/dp/Knapsack2.java
@@ -0,0 +1,70 @@
+import java.lang.Math;
+
+public class Knapsack2 {
+    private int []mWeights;
+    private int []mValues;
+    private int mCapacity;
+    
+    public Knapsack2(int[] weights, int []values, int capacity) {
+	mWeights = weights;
+	mValues = values;
+	mCapacity = capacity;
+    }
+    
+    /*
+      Knapsack algorithm - Same as Knapsack.java
+      except evaluates string starting at end of the srting.
+      
+      This algorithm treats i as a 1-based index. getValue/getWeight
+      abstract indexing into mValues/mWeights to make the algorithm
+      easier to follow. eg - if capacity is 7 and number of items
+      to evaluate is 4, the method would be invoked like this the first time:
+
+      fillSackRecursive(7, 4);
+
+    */
+    public int fillSack() {
+        int [][]table = new int[mCapacity+1][mWeights.length];
+	for (int i = 0; i < table.length; ++i) {
+	    for (int j = 0; j < table[0].length; ++j) {
+		table[i][j] = -1;
+	    }
+	}
+
+	return fillSackRecursive(mCapacity, mWeights.length, table);
+    }
+    
+    private int fillSackRecursive(int c, int i, int [][]table) {
+	if (c <= 0 || i == 0)
+	    return 0;
+
+	if (getWeight(i) > c) {
+	    if (table[c][i-1] == -1)
+		table[c][i-1] = fillSackRecursive(c, i-1, table);
+
+	    return table[c][i-1];
+
+	} else {
+	    if (table[c][i-1] == -1)
+		table[c][i-1] = fillSackRecursive(c, i-1, table);
+	    
+	    int unselected = table[c][i-1];
+	    
+	    int reducedCapacity = c - getWeight(i);
+	    if (table[reducedCapacity][i-1] == -1) 
+		table[reducedCapacity][i-1] = fillSackRecursive(reducedCapacity, i-1, table);
+
+	    int selected = getValue(i) + table[reducedCapacity][i-1];
+
+	    return Math.max(unselected, selected);
+	}
+    }
+     
+    private int getWeight(int i) {
+	return mWeights[i-1];
+    }
+    
+    private int getValue(int i) {
+	return mValues[i-1];
+    }
+}
