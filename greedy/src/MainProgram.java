ccaae486831504fa17bcab5e114150977699de3d
Ryan Reeves <rreevessde@gmail.com>
2011-06-25 23:47:10 -0700

    Renaming source file

--- greedy/src/MainProgram.java
+++ greedy/src/MainProgram.java
@@ -1,6 +1,5 @@
-
 public class MainProgram {
-    public static void main(String []args) {
+    private static void knapSackTest() {
 	KnapsackItem []items = new KnapsackItem[4];
 	items[0] = new KnapsackItem(7, 3);
 	items[1] = new KnapsackItem(5, 2);
@@ -14,5 +13,45 @@ public class MainProgram {
 	
 	System.out.println(amount);
 	System.out.println(amount2);
+
+    }
+    
+    private static void shortestPathTest() {
+	Graph graph = new Graph(6);
+	
+	//Add vertices
+	graph.addVertex(0, "A");
+	graph.addVertex(1, "B");
+	graph.addVertex(2, "C");
+	graph.addVertex(3, "D");
+	graph.addVertex(4, "E");
+	graph.addVertex(5, "F");
+	
+	//A edges
+	graph.addEdge(0, 1, 1);
+	graph.addEdge(0, 2, 5);
+	
+	//B edges
+	graph.addEdge(1, 3, 6);
+	
+	//C edges
+	graph.addEdge(2, 3, 1);
+	graph.addEdge(2, 4, 1);
+	
+        //D edges
+	graph.addEdge(3, 5, 7);
+	
+	//E edges
+	graph.addEdge(4, 5, 0);
+	
+	//F has no edges
+
+	DijkstraShortestPath shortestPath = new DijkstraShortestPath();
+	shortestPath.printPath(graph, 0, 5);
+    }
+
+    public static void main(String []args) {
+	knapSackTest();
+	shortestPathTest();
     }
 }
